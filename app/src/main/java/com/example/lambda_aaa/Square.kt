8ee604c7dcde74c011423b8e1f537721f087877d
Chandler Qian <chandler20060524@gmail.com>
2026-09-24 18:39:42 -0600

    Added color private field to Square class

--- app/src/main/java/com/example/lambda_aaa/Square.kt
+++ app/src/main/java/com/example/lambda_aaa/Square.kt
@@ -2,6 +2,7 @@ package com.example.lambda_aaa
 
 class Square(x: Int, y: Int): Shape(x, y) {
     private val area: Int = x * y
+    private val color: String = "emerald"
 
     fun getArea(): Int {
         return area
